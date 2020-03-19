6f4d5b64ca8d93912f0083887cda4dc3c55be4b9
cerber-os <contact@cerberos.pl>
2020-03-19 21:41:07 +0100

    Omit already generated thumbnails

--- wirebackupparser/utils.py
+++ wirebackupparser/utils.py
@@ -12,7 +12,7 @@ def generateThumbnail(inputFile, outputFile):
                       (im.size[0] + crop) // 2,
                       (im.size[1] + crop) // 2))
         im.thumbnail((100, 100))
-        im.save(outputFile + '.png')
+        im.save(outputFile)
     except OSError:
         return
         # TODO: Add additional file formats
@@ -22,5 +22,6 @@ def genThumbsForFilesInDir(inputDir, outputDir):
     print("Generating thumbnails...")
     for fileName in tqdm(os.listdir(inputDir)):
         file = os.path.join(inputDir, fileName)
-        if os.path.isfile(file):
-            generateThumbnail(file, os.path.join(outputDir, fileName))
+        out = os.path.join(outputDir, fileName) + '.png'
+        if os.path.isfile(file) and not os.path.exists(out):
+            generateThumbnail(file, out)
